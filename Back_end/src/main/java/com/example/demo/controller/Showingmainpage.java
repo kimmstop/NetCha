c84d2e35e390f3fe782575f2e68aef60bd66c9b1
kimmstop <46364133+kimmstop@users.noreply.github.com>
2021-11-02 21:01:58 +0900

    Back_end

---
+++ Back_end/src/main/java/com/example/demo/controller/Showingmainpage.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Showingmainpage {
+    @GetMapping("/netcha")
+    public String showmainpage(){
+        return "모두들 화이팅!";
+    }
+}
